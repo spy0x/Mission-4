225dc8b15bbd71542357b3d1fbdf973d6c1ff6ff
Francisco V. Diaz <fevarga1@uc.cl>
2021-11-23 09:59:35 -0300

    added script ShapeBehaviour to Shapes. Initialized and declared first variables and methods for parent class.

--- Assets/Scripts/ShapeBehaviour.cs
+++ Assets/Scripts/ShapeBehaviour.cs
@@ -1,7 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShapeBehaviour : MonoBehaviour
 {
+    [SerializeField] private Color32 color;
+    private int shapeClicks = 0;
+    private static int allShapesTotalClicks;
+    public static int AllShapesTotalClicks { get { return allShapesTotalClicks; } set { allShapesTotalClicks = value; } }
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+    }
+    private void OnMouseDown()
+    {
+        DoAction();
+        AddClickCount();
+    }
+
+    private void AddClickCount()
+    {
+        allShapesTotalClicks++;
+        shapeClicks++;
+    }
+
+    protected virtual void DoAction()
+    {
+        Debug.Log("Override this method please");
+    }
 }
