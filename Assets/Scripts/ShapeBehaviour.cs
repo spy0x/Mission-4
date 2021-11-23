93620439bd38ba485848ec23b9962818f76f7f66
Francisco V. Diaz <fevarga1@uc.cl>
2021-11-23 12:48:17 -0300

    added total clicks in UI

--- Assets/Scripts/ShapeBehaviour.cs
+++ Assets/Scripts/ShapeBehaviour.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShapeBehaviour : MonoBehaviour
 {
     [ColorUsage(false, false)]
     [SerializeField] protected Color color;
+    [SerializeField] private TextMeshProUGUI totalClicks;
     private int shapeClicks = 0;
     protected MeshRenderer meshRenderer;
     private static int allShapesTotalClicks;
     public static int AllShapesTotalClicks { get { return allShapesTotalClicks; } set { allShapesTotalClicks = value; } }
     protected virtual void Start()
     {
+        UpdateUI();
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material.color = color;
     }
@@ -29,7 +32,13 @@ public class ShapeBehaviour : MonoBehaviour
     {
         allShapesTotalClicks++;
         shapeClicks++;
+        UpdateUI();
     }
+    private void UpdateUI()
+    {
+        totalClicks.text = $"Total Clicks: {allShapesTotalClicks}";
+    }
+
     protected virtual void DoActionOnDown()
     {
         Debug.Log("Override this method please");
